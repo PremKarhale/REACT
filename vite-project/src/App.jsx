711316d78980a1df468ea84072b661e61a0697d8
Prem Karhale <premkarhale383@gmail.com>
2025-07-18 15:51:12 +0530

    APP.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 
-
-
+// this is a COMPONENT  IN JSX WRITTEN IN <App/>
 function App() {
   return (
-<h1> hey now after javascript it's time for React </h1>
+    <h1> hey now after javascript it's time for React </h1> 
+  
   )
 }
 
