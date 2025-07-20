3b482e0266acb2146269d6bc2343794e8238532f
Prem Karhale <premkarhale383@gmail.com>
2025-07-20 22:26:37 +0530

    src

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,25 +1,10 @@
-import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import './index.css'
 import App from './App.jsx'
 
-const ReactElement = React.createElement(
-  'a', //set tag name
-
-  { href: "https://google.com", target: '_blank' }, //set attributes 
-
-  'click on me to visit google '// inner html or children
-
-)
-
-
-// <strict mode > --> likha to likha nahi to bhi chalta hai 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
-   <App />
-    {ReactElement}  {/* because it is not a component that is why we use {}  */}
-
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
